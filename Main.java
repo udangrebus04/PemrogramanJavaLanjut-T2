dfbaea0ac622bc24d5e3ab25f6147702a3fa08e6
udangrebus04 <cellomitaputri@gmail.com>
2026-04-01 16:22:55 +0700

    revisi

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@ public class Main {
         Penumpang p1 = new Penumpang ("001", "Cellomita", 20);
         Penumpang p2 = new Penumpang ("002", "Hasyifa", 25);
 
-        Tiket t1 = new TiketEkonomi("T1", "Jakarta",100000, p1);
-        Tiket t2 = new TiketEksekutif("T2", "Bandung",100000, p2);
+        Tiket t1 = new TiketEkonomi("Ekonomi", "Jakarta",100000, p1);
+        Tiket t2 = new TiketEksekutif("Eksekutif", "Bandung",100000, p2);
 
         Pembayaran bayar1 = new BayarTunai();
         Pembayaran bayar2 = new BayarTransfer();
